414e22b4d6b77dbba94e183ee2a3be45a5a35404
Mikhaiel_Offical <72667834+realmikhaiel@users.noreply.github.com>
2021-10-18 11:11:52 +0530

    📗

--- plugins/hmod.js
+++ plugins/hmod.js
@@ -4,6 +4,7 @@ you may not use this file except in compliance with the License.
 WhatsAsenaDuplicated*/
 
 
+*/*
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
@@ -49,3 +50,84 @@ Asena.addCommand({pattern: 'modapp ?(.*)', fromMe: false, desc: 'get mod apk fro
 	}
 });
 }
+*/*
+	
+ const jimbru = require('../events');
+
+ const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+
+ const fs = require('fs');
+
+ const ffmpeg = require('fluent-ffmpeg');
+
+ const { execFile } = require('child_process');
+
+ const Config = require('../config');
+
+          var mode = ''
+
+if (Config.OWNRN == 'default') mode = '𝘫𝘪𝘮𝘣𝘳𝘰𝘰𝘵𝘵𝘢𝘯'
+
+else mode = '𝘧𝘰𝘳𝘸𝘢𝘳𝘥𝘦𝘥 𝘣𝘺 ' + Config.OWNRN
+
+            const jimbru = {
+
+                  key: {"fromMe": false,"participant": "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "62895619083555-1616169743@g.us","inviteCode": "mememteeeekkeke","groupName": "jimbroottan", "caption": mode , 'jpegThumbnail': fs.readFileSync('./Media/image/logo.jpg')}}
+
+
+
+
+
+            }
+
+Rashi.addcommand({pattern: 'mforward ?(.*)', fromMe: true, desc: 'its foraward replied audio'}, (async (message, match) => {    
+
+    if (message.reply_message === false);
+
+
+
+    if(message.reply_message.video ||
+
+      message.reply_message.sticker ||
+
+      message.reply_message.pdf
+
+    )
+
+      return message.sendMessage("*Not supported!*\n\n  𝘰𝘯𝘭𝘺 𝘴𝘶𝘱𝘱𝘰𝘳𝘵 𝘢𝘶𝘥𝘪𝘰 𝘧𝘪𝘭𝘦");
+
+
+
+        if (match == "") return await message.sendMessage("*Give me a jid*\nExample .mforward jid1 jid2 jid3 jid4 ...");
+
+
+
+    var location = await message.client.downloadAndSaveMediaMessage({
+
+        key: {
+
+            remoteJid: message.reply_message.jid,
+
+            id: message.reply_message.id
+
+        },
+
+        message: message.reply_message.data.quotedMessage
+
+    });
+
+let id = match[1];
+
+    ffmpeg(location)
+
+        .format('mp3')
+
+        .save('output.mp3')
+
+        .on('end', async () => {
+
+            await message.client.sendMessage(id, fs.readFileSync('output.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true, quoted: jimbru,contextInfo: { forwardingScore: 2, isForwarded: true}});
+
+});
+
+}));
